4aa950a440baa874a559b32210ddaffce04b3949
jimchen5209 <jimchen5209@gmail.com>
2020-05-05 16:19:17 +0800

    Add reload command and enhance messages

--- Components/Discord/Components/Text.ts
+++ Components/Discord/Components/Text.ts
@@ -50,10 +50,15 @@ export class DiscordText {
             usage: '<userID>',
         });
 
-        this.bot.registerCommand('showlist', this.commandShowList.bind(this), {
+        this.bot.registerCommand('showList', this.commandShowList.bind(this), {
             description: 'Show sound list',
             guildOnly: true,
-        })
+        });
+
+        this.bot.registerCommand('reload', this.commandReload.bind(this), {
+            description: 'Reload sound list',
+            guildOnly: true,
+        });
     }
 
     private handleSoundPlay(msg: Message) {
@@ -86,7 +91,32 @@ export class DiscordText {
             embed: {
                 title: 'Sound Available',
                 color: 10666230,
-                description: `Commands:\n${commandList}\nKeywords:\n${this.sound.getSoundKeyWordList().join('\n')}`
+                description: `**Commands:**\n${commandList}\n\n**Keywords:**\n${this.sound.getSoundKeyWordList().join('\n')}`
+            }
+        });
+    }
+
+    private async commandReload(msg: Message) {
+        if (!msg.member) return;
+
+        const message = await msg.channel.createMessage({
+            embed: {
+                title: 'Reloading sound list...',
+                color: 16312092,
+                description: `Please wait...`
+            }
+        })
+
+        this.sound.reload();
+
+        const newCommandCount = this.sound.getSoundCommandList().length;
+        const newKeyWordCount = this.sound.getSoundKeyWordList().length;
+
+        message.edit({
+            embed: {
+                title: 'Reload sound list done.',
+                color: 4289797,
+                description: `There ${((newCommandCount + newKeyWordCount) <= 1) ? 'is' : 'are'} now ${newCommandCount} command${(newCommandCount <= 1) ? '' : 's'} and ${newKeyWordCount} keyword${(newKeyWordCount <= 1) ? '' : 's'} loaded.\nUse "${this.bot.user.mention} showList" to view it.`
             }
         });
     }
@@ -96,12 +126,24 @@ export class DiscordText {
 
         const voiceChannelID = msg.member.voiceState.channelID;
         if (voiceChannelID === undefined) {
-            msg.channel.createMessage(ERR_NOT_IN_VOICE_CHANNEL);
+            msg.channel.createMessage({
+                embed: {
+                    title: 'Error',
+                    color: 13632027,
+                    description: ERR_NOT_IN_VOICE_CHANNEL
+                }
+            });
             return;
         }
 
         if (this.audios[voiceChannelID] === undefined) {
-            msg.channel.createMessage(ERR_BOT_NOT_IN_VOICE_CHANNEL);
+            msg.channel.createMessage({
+                embed: {
+                    title: 'Error',
+                    color: 13632027,
+                    description: ERR_BOT_NOT_IN_VOICE_CHANNEL
+                }
+            });
             return;
         }
 
@@ -110,7 +152,13 @@ export class DiscordText {
         if (buffer !== undefined) {
             msg.channel.createMessage('', { file: buffer, name: `${userID}.mp3` } as MessageFile);
         } else {
-            msg.channel.createMessage(ERR_NO_DATA);
+            msg.channel.createMessage({
+                embed: {
+                    title: 'Error',
+                    color: 13632027,
+                    description: ERR_NO_DATA
+                }
+            });
         }
     }
 }

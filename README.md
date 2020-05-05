bb2044a49de4357a77900db62d244f0ee9fd4e2d
jimchen5209 <jimchen5209@gmail.com>
2020-05-05 15:44:56 +0800

    Add readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# fx_record_ts_bot
+
+## How to use
+
+1. Install [node.js](https://nodejs.org/en/) (Recommend using [nvm](https://github.com/nvm-sh/nvm)),, [ffmpeg](https://ffmpeg.org/
+2. Clone this repo
+3. Install dependencies with `npm install`
+4. Build with `npm run build:prod`
+5. Run `npm run installMixer` to install Native C++ Mixer (Optional but recommended)
+6. Run `npm run start` the first time to generate `config.json`
+7. Create and grab your discord bot token [here](https://discordapp.com/developers/applications/)
+8. Fill `config.json`
+9. Install `pm2` via `npm install -g pm2` (Optional but recommended)
+10. Start the bot with `npm run start` or `pm2 reload ecosystem.config.js`
---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    apps: [{
+        name: 'fx_record_ts_bot',
+        script: 'npm',
+        // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+        args: 'run start',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '512M',
+	cron_restart: '0 17 * * *',
+        env_production: {
+            NODE_ENV: 'production'
+        }
+    }],
+
+    deploy: {
+        production: {
+            user: 'node',
+            ref: 'origin/master',
+            repo: 'https://github.com/jimchen5209/fx_record_ts_bot.git',
+            path: '/var/www/production',
+            'post-deploy': 'npm install && npm run installMixer && pm2 reload ecosystem.config.js --env production'
+        }
+    }
+};

2d2cff32e8c23920eb473248f6fc822c7bb2c484
jimchen5209 <jimchen5209@gmail.com>
2021-06-27 09:17:15 +0800

    Disable/remove unused methods

--- Components/Discord/Components/Text.ts
+++ Components/Discord/Components/Text.ts
@@ -1,8 +1,8 @@
-import { CommandClient, Message, TextChannel, PossiblyUncachedTextableChannel } from 'eris'; //MessageFile, MessageContent,
+import { CommandClient, TextChannel } from 'eris'; //MessageFile, MessageContent, Message,, PossiblyUncachedTextableChannel
 import { Category } from 'logging-ts';
-import { DiscordVoice } from './Voice';
+// import { DiscordVoice } from './Voice';
 import { Discord } from '../Core';
-import { SoundFx } from '../../../Core/SoundFX';
+// import { SoundFx } from '../../../Core/SoundFX';
 import { Core } from '../../..';
 
 // const ERR_NOT_IN_VOICE_CHANNEL = 'You are not in any voice channel.';
@@ -12,14 +12,14 @@ import { Core } from '../../..';
 export class DiscordText {
     private bot: CommandClient;
     private logger: Category;
-    private audios: { [key: string]: DiscordVoice } = {};
-    private sound: SoundFx;
+    // private audios: { [key: string]: DiscordVoice } = {};
+    // private sound: SoundFx;
 
     constructor(core: Core, discord: Discord, bot: CommandClient, logger: Category) {
         this.bot = bot;
         this.logger = logger;
-        this.audios = discord.audios;
-        this.sound = new SoundFx(core);
+        // this.audios = discord.audios;
+        // this.sound = new SoundFx(core);
 
         this.bot.on('messageCreate', msg => {
 
@@ -36,90 +36,90 @@ export class DiscordText {
             messageContent.split('\n').forEach(content => {
                 this.logger.info(`${userNick}[${userName}, ${userID}] => ${channelName} (${channelID}): ${content}`);
             });
-            if (!msg.member.bot) this.handleSoundPlay(msg);
+            // if (!msg.member.bot) this.handleSoundPlay(msg);
         });
 
-        this.registerCommand();
+        // this.registerCommand();
     }
 
-    private registerCommand() {
-        // this.bot.registerCommand('download', this.commandDownload.bind(this), {
-        //     argsRequired: true,
-        //     description: 'Download user\'s voice',
-        //     guildOnly: true,
-        //     usage: '<userID>',
-        // });
+    // private registerCommand() {
+    //     // this.bot.registerCommand('download', this.commandDownload.bind(this), {
+    //     //     argsRequired: true,
+    //     //     description: 'Download user\'s voice',
+    //     //     guildOnly: true,
+    //     //     usage: '<userID>',
+    //     // });
 
-        this.bot.registerCommand('showList', this.commandShowList.bind(this), {
-            description: 'Show sound list',
-            guildOnly: true,
-        });
+    //     // this.bot.registerCommand('showList', this.commandShowList.bind(this), {
+    //     //     description: 'Show sound list',
+    //     //     guildOnly: true,
+    //     // });
 
-        this.bot.registerCommand('reload', this.commandReload.bind(this), {
-            description: 'Reload sound list',
-            guildOnly: true,
-        });
-    }
+    //     // this.bot.registerCommand('reload', this.commandReload.bind(this), {
+    //     //     description: 'Reload sound list',
+    //     //     guildOnly: true,
+    //     // });
+    // }
 
-    private handleSoundPlay(msg: Message<PossiblyUncachedTextableChannel>) {
-        if (!msg.member) return;
+    // private handleSoundPlay(msg: Message<PossiblyUncachedTextableChannel>) {
+    //     if (!msg.member) return;
 
-        const voiceChannelID = msg.member.voiceState.channelID;
-        if (!voiceChannelID) return;
-        if (this.audios[voiceChannelID] === undefined) return;
+    //     const voiceChannelID = msg.member.voiceState.channelID;
+    //     if (!voiceChannelID) return;
+    //     if (this.audios[voiceChannelID] === undefined) return;
 
-        msg.content.split('\n').forEach(async command => {
-            const commandWithArgs = command.split(' ');
+    //     msg.content.split('\n').forEach(async command => {
+    //         const commandWithArgs = command.split(' ');
 
-            if (commandWithArgs[0] === `<@!${this.bot.user.id}>`) {
-                const sound = this.sound.getAssetFromCommand(commandWithArgs[1]);
-                if (sound !== undefined) this.audios[voiceChannelID].playSound(sound);
-                return;
-            }
-            this.sound.getSoundKeyWordList().forEach(word => {
-                if (command.includes(word)) this.audios[voiceChannelID].playSound(this.sound.getAssetFromKeyWord(word));
-            });
-        });
-    }
+    //         if (commandWithArgs[0] === `<@!${this.bot.user.id}>`) {
+    //             const sound = this.sound.getAssetFromCommand(commandWithArgs[1]);
+    //             if (sound !== undefined) this.audios[voiceChannelID].playSound(sound);
+    //             return;
+    //         }
+    //         this.sound.getSoundKeyWordList().forEach(word => {
+    //             if (command.includes(word)) this.audios[voiceChannelID].playSound(this.sound.getAssetFromKeyWord(word));
+    //         });
+    //     });
+    // }
 
-    private async commandShowList(msg: Message) {
-        if (!msg.member) return;
+    // private async commandShowList(msg: Message) {
+    //     if (!msg.member) return;
 
-        const commandList = this.sound.getSoundCommandList().map(value => `${this.bot.user.mention} ${value}`).join('\n');
+    //     const commandList = this.sound.getSoundCommandList().map(value => `${this.bot.user.mention} ${value}`).join('\n');
 
-        msg.channel.createMessage({
-            embed: {
-                title: 'Sound Available',
-                color: 10666230,
-                description: `**Commands:**\n${commandList}\n\n**Keywords:**\n${this.sound.getSoundKeyWordList().join('\n')}`
-            }
-        });
-    }
+    //     msg.channel.createMessage({
+    //         embed: {
+    //             title: 'Sound Available',
+    //             color: 10666230,
+    //             description: `**Commands:**\n${commandList}\n\n**Keywords:**\n${this.sound.getSoundKeyWordList().join('\n')}`
+    //         }
+    //     });
+    // }
 
-    private async commandReload(msg: Message) {
-        if (!msg.member) return;
+    // private async commandReload(msg: Message) {
+    //     if (!msg.member) return;
 
-        const message = await msg.channel.createMessage({
-            embed: {
-                title: 'Reloading sound list...',
-                color: 16312092,
-                description: 'Please wait...'
-            }
-        });
+    //     const message = await msg.channel.createMessage({
+    //         embed: {
+    //             title: 'Reloading sound list...',
+    //             color: 16312092,
+    //             description: 'Please wait...'
+    //         }
+    //     });
 
-        this.sound.reload();
+    //     this.sound.reload();
 
-        const newCommandCount = this.sound.getSoundCommandList().length;
-        const newKeyWordCount = this.sound.getSoundKeyWordList().length;
+    //     const newCommandCount = this.sound.getSoundCommandList().length;
+    //     const newKeyWordCount = this.sound.getSoundKeyWordList().length;
 
-        message.edit({
-            embed: {
-                title: 'Reload sound list done.',
-                color: 4289797,
-                description: `There ${((newCommandCount + newKeyWordCount) <= 1) ? 'is' : 'are'} now ${newCommandCount} command${(newCommandCount <= 1) ? '' : 's'} and ${newKeyWordCount} keyword${(newKeyWordCount <= 1) ? '' : 's'} loaded.\nUse "${this.bot.user.mention} showList" to view it.`
-            }
-        });
-    }
+    //     message.edit({
+    //         embed: {
+    //             title: 'Reload sound list done.',
+    //             color: 4289797,
+    //             description: `There ${((newCommandCount + newKeyWordCount) <= 1) ? 'is' : 'are'} now ${newCommandCount} command${(newCommandCount <= 1) ? '' : 's'} and ${newKeyWordCount} keyword${(newKeyWordCount <= 1) ? '' : 's'} loaded.\nUse "${this.bot.user.mention} showList" to view it.`
+    //         }
+    //     });
+    // }
 
     // private async commandDownload(msg: Message, args: string[]) {
     //     if (!msg.member) return;
--- Components/Discord/Components/Voice.ts
+++ Components/Discord/Components/Voice.ts
@@ -13,14 +13,12 @@ export class DiscordVoice {
     private bot: CommandClient;
     private logger: Category;
     private channelConfig: { id: string, fileDest: { type: string, id: string }, ignoreUsers: string[] };
-    // private playMixer = new LicsonMixer(16, 2, 48000);
     private recvMixer = new LicsonMixer(16, 2, 48000);
     // private userMixers: { [key: string]: LicsonMixer } = {};
     // private userRawPCMStreams: { [key: string]: Stream.PassThrough } = {};
     // private userRawMP3Streams: { [key: string]: Stream.PassThrough } = {};
     // private userMP3Buffers: { [key: string]: any[] } = {};
     private telegramSendInterval: NodeJS.Timeout | undefined;
-    // private clearStreamInterval!: NodeJS.Timeout;
 
     constructor(
         core: Core,
@@ -36,9 +34,9 @@ export class DiscordVoice {
         this.startAudioSession(this.channelConfig.id);
     }
 
-    public getUserMP3Buffer(userID: string) {
-        // return (this.userMP3Buffers[userID] !== undefined) ? Buffer.concat(this.userMP3Buffers[userID]) : undefined;
-    }
+    // public getUserMP3Buffer(userID: string) {
+    // return (this.userMP3Buffers[userID] !== undefined) ? Buffer.concat(this.userMP3Buffers[userID]) : undefined;
+    // }
 
     private startAudioSession(channelID: string) {
         this.joinVoiceChannel(channelID).then(connection => {
@@ -57,66 +55,40 @@ export class DiscordVoice {
     //     });
     // }
 
-    public playSound(sound: string) {
-        // this.logger.info(`Play ${sound} in voice channel ${this.channelConfig.id}} `);
+    // public playSound(sound: string) {
+    // this.logger.info(`Play ${sound} in voice channel ${this.channelConfig.id}} `);
 
-        // const playStream = AudioUtils.soundFileStreamGenerator(sound, this.core.config.debug);
-        // AudioUtils.addStreamToChannelPlayMixer(playStream, this.playMixer);
-    }
+    // const playStream = AudioUtils.soundFileStreamGenerator(sound, this.core.config.debug);
+    // AudioUtils.addStreamToChannelPlayMixer(playStream, this.playMixer);
+    // }
 
     private startRecording(connection: VoiceConnection) {
-        connection.receive('pcm').on('data', (data, user, time) => {
-            if (user === undefined) return;
-            // console.log(`User: ${user}, time: ${time}`);
+        connection.receive('pcm').on('data', (data, user) => {
+            // if (!user || this.channelConfig.ignoreUsers.includes(user)) return;
+            if (!user) return;
+
             let source = this.recvMixer.getSources(user)[0];
-            if (!source) source = this.recvMixer.addSource(new AbortStream(64 * 1000 * 8, 64 * 1000 * 4), user);
+            if (!source) {
+                this.logger.info(`New user ${user} to record mixer ${this.channelConfig.id}.`);
+                source = this.recvMixer.addSource(new AbortStream(64 * 1000 * 8, 64 * 1000 * 4), user);
+            }
             source.stream.write(data);
+
+            // if (!this.userMixers[user]) this.newPerUserMixer(user);
+
+            // let perUserSource = this.userMixers[user].getSources(user)[0];
+            // if (!source) perUserSource = this.userMixers[user].addSource(new AbortStream(64 * 1000 * 8, 64 * 1000 * 4), user);
+            // perUserSource.stream.write(data);
         });
-
-        // this.addNewUserToMixer(this.bot.user.id);
-
-        // (this.playMixer as unknown as Stream.Readable).on('data', (data: any) => {
-        //     // if (!this.userRawPCMStreams[this.bot.user.id].write(data)) {
-        //     //     console.log("userRawPCMStreams write false")
-        //     // };
-        //     if (!this.userRawMP3Streams[this.bot.user.id].write(data)) {
-        //         // console.log("userRawMP3Streams write false")
-        //     };
-        // });
-
-        // recvStream.on('data', (data, userID, timestamp, sequence) => {
-        //     if (userID === undefined || this.channelConfig.ignoreUsers.includes(userID)) return;
-
-        //     if (false || this.userRawMP3Streams[userID] === undefined) {
-        //         this.addNewUserToMixer(userID);
-        //     }
-        //     // if (!this.userRawPCMStreams[userID].write(data)){
-        //     //     console.log("userRawPCMStreams write false 2")
-        //     // };
-        //     if (!this.userRawMP3Streams[userID].write(data)) {
-        //         // console.log("userRawMP3Streams write false 2")
-        //     };
-        // });
     }
 
-    // private addNewUserToMixer(userID: string) {
-    //     this.logger.info(`New user ${userID} to record mixer ${this.channelConfig.id}.`);
-    //     const userMixer = this.userMixers[userID] = new LicsonMixer(16, 2, 48000);
-    //     const userMP3Buffer: any[] = this.userMP3Buffers[userID] = [];
-    //     // this.userRawPCMStreams[userID] = new Stream.PassThrough();
-    //     this.userRawMP3Streams[userID] = new Stream.PassThrough();
-
-    //     userMixer.addSource(this.userRawMP3Streams[userID], []);
-    //     // this.recvMixer.addSource(this.userRawPCMStreams[userID], []);
-
-    //     // AudioUtils.generatePCMtoMP3Stream(userMixer, this.core.config.debug).on('data', (mp3Data: any) => {
-    //     //     userMP3Buffer.push(mp3Data);
-    //     //     if (userMP3Buffer.length > 4096) userMP3Buffer.splice(0, userMP3Buffer.length - 4096);
-    //     // });
+    // private newPerUserMixer(user: string) {
+    //     this.logger.info(`New per user mixer ${user} for ${this.channelConfig.id} created.`);
+    //     this.userMixers[user] = new LicsonMixer(16, 2, 48000);
     // }
 
     private startSendRecord() {
-        if (this.channelConfig.fileDest.type === 'telegram' && this.channelConfig.fileDest.id !== '' && this.core.telegram !== undefined) {
+        if (this.channelConfig.fileDest.type === 'telegram' && this.channelConfig.fileDest.id !== '' && this.core.telegram) {
             const mp3Stream = AudioUtils.generatePCMtoMP3Stream(this.recvMixer, this.core.config.debug);
 
             let mp3Start = moment().tz('Asia/Taipei').format('YYYY-MM-DD hh-mm-ss');
@@ -134,7 +106,7 @@ export class DiscordVoice {
                 const caption = `${mp3Start} -> ${mp3End} \n${moment().tz('Asia/Taipei').format('#YYYYMMDD #hhmm #YYYY')}`;
 
                 this.logger.info(`Sending ${mp3Start}.mp3 of ${this.channelConfig.id} to telegram ${this.channelConfig.fileDest.id}`);
-                this.core.telegram?.sendAudio(this.channelConfig.fileDest.id, `temp/${this.channelConfig.id}/${mp3Start}.mp3`, caption).then(i => unlinkSync(i));
+                if (this.core.telegram) this.core.telegram.sendAudio(this.channelConfig.fileDest.id, `temp/${this.channelConfig.id}/${mp3Start}.mp3`, caption).then(i => unlinkSync(i));
                 mp3Start = moment().tz('Asia/Taipei').format('YYYY-MM-DD hh-mm-ss');
                 writeStream = createWriteStream(`temp/${this.channelConfig.id}/${mp3Start}.mp3`);
                 mp3Stream.pipe(writeStream);
@@ -154,22 +126,8 @@ export class DiscordVoice {
         // }
 
         if (this.telegramSendInterval !== undefined) clearInterval(this.telegramSendInterval);
-        // clearInterval(this.clearStreamInterval);
         this.bot.leaveVoiceChannel(channelID);
     }
-
-    // private clearStreams() {
-    //     for (const user in Object.keys(this.userRawMP3Streams)) {
-    //         if (this.userRawMP3Streams[user]) {
-    //             this.endStream(user);
-    //         }
-    //     }
-    //     for (const user in Object.keys(this.userRawPCMStreams)) {
-    //         if (this.userRawPCMStreams[user]) {
-    //             this.endStream(user);
-    //         }
-    //     }
-    // }
 
     private async joinVoiceChannel(channelID: string): Promise<VoiceConnection> {
         this.logger.info(`Connecting to ${channelID}...`);
@@ -190,23 +148,15 @@ export class DiscordVoice {
         return connection;
     }
 
-    private endStream(userID: string) {
-        const source = this.recvMixer.getSources(userID)[0];
+    private endStream(user: string) {
+        const source = this.recvMixer.getSources(user)[0];
         if (source) source.stream.end();
-        // if (this.userRawPCMStreams[userID]) {
-        //     this.userRawPCMStreams[userID].end();
-        //     delete this.userRawPCMStreams[userID];
-        // }
-        // if (this.userRawMP3Streams[userID]) {
-        //     this.userRawMP3Streams[userID].end();
-        //     delete this.userRawMP3Streams[userID];
-        // }
     }
 
     private setEndStreamEvents(connection: VoiceConnection) {
         const guildID = (this.bot.getChannel(this.channelConfig.id) as VoiceChannel).guild.id;
-        connection.on('userDisconnect', userID => {
-            this.endStream(userID);
+        connection.on('userDisconnect', user => {
+            this.endStream(user);
         });
 
         this.bot.on('voiceChannelSwitch', (member, newChannel) => {
@@ -216,10 +166,4 @@ export class DiscordVoice {
             }
         });
     }
-
-    // private startClearStreamInterval() {
-    //     this.clearStreamInterval = setInterval(() => {
-    //         this.clearStreams();
-    //     }, 10 * 60 * 1000);
-    // }
 }

80382695eee888cb5b175cc7c84860d468485b69
james58899 <james59988@gmail.com>
2021-06-30 19:52:52 +0800

    Push

--- Components/Discord/Components/Silence.ts
+++ Components/Discord/Components/Silence.ts
@@ -5,6 +5,6 @@ const SILENCE_FRAME = Buffer.from([0xF8, 0xFF, 0xFE]);
 export class Silence extends Stream.Readable {
     _read() {
         this.push(SILENCE_FRAME);
-        this.destroy();
+        this.push(null);
     }
 }

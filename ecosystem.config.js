5a1697f20235b6bcb682146a14ce3888e77d5007
jimchen5209 <jimchen5209@gmail.com>
2021-07-01 23:12:20 +0800

    Update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,15 @@
 module.exports = {
     apps: [{
         name: 'fx_record_ts_bot',
-        script: 'npm',
+        script: './dist',
+        // script: 'npm',
         // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-        args: 'run start',
+        // args: 'run start',
         instances: 1,
         autorestart: true,
         watch: false,
         max_memory_restart: '512M',
-        cron_restart: '0 17 * * *',
+        // cron_restart: '0 17 * * *',
         env_production: {
             NODE_ENV: 'production'
         }
@@ -20,7 +21,7 @@ module.exports = {
             ref: 'origin/master',
             repo: 'https://github.com/jimchen5209/fx_record_ts_bot.git',
             path: '/var/www/production',
-            'post-deploy': 'npm install && npm run installMixer && pm2 reload ecosystem.config.js --env production'
+            'post-deploy': 'yarn install && yarn build:prod && pm2 reload ecosystem.config.js --env production'
         }
     }
 };
